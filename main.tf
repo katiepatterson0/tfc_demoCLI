1848323622f718653667da47710f7695439251e3
Patterson Krise <patterson@hashicorp.com>
2024-04-04 15:49:16 -0400

    c

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my_other_app_server" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Example EC2 for demo from CLI"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+    organization = "patterson-demo"
+
+    workspaces {
+      name = "tfc_demo2"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0.1, <5.43.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws-access-key
+  secret_key = var.aws-secret-access-key
+}
