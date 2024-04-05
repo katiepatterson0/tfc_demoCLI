2367d2d811c918d31814a95bfc0e11e63a7876b3
Patterson Krise <patterson@hashicorp.com>
2024-04-05 13:14:30 -0400

    c

--- providers.tf
+++ providers.tf
@@ -1,4 +1,18 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0.1, <5.43.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws-access-key
+  secret_key = var.aws-secret-access-key
+}
+
+terraform {
   cloud {
     organization = "patterson-demo"
 
@@ -6,16 +20,4 @@ terraform {
       name = "tfc_democli"
     }
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=5.0.1, <5.43.0"
-    }
-  }
-
-provider "aws" {
-  region = "us-east-1"
-  access_key = var.aws-access-key
-  secret_key = var.aws-secret-access-key
-}
 }
