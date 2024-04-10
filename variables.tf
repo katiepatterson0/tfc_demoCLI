722d7dbf0944887d69f584a266fbb4e48effe81d
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-10 17:51:24 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,3 @@ variable "aws-secret-access-key" {
     type = string
     description = "aws secret access key"
 }
-variable "ec2_name" {
-    type = string
-    description = "Name of EC2 instance"
-}
