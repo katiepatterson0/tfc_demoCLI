39d0cfaa94c6323b967b12875ed95457b4d596f9
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-11 13:21:35 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,12 +11,4 @@ provider "aws" {
   access_key = var.aws-access-key
   secret_key = var.aws-secret-access-key
 }
-terraform {
-  cloud {
-    organization = "patterson-demo"
 
-    workspaces {
-      name = "cliapr11"
-    }
-  }
-}
